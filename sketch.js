5396e0be30993d3acdbbc3b6ace3dc841cc2aa08
ParelZalm <62101020+ParelZalm@users.noreply.github.com>
2022-05-05 23:38:43 +0200

    add sound file to bubble

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 let mic, recorder, soundFile;
 let state = 0;
+let numberFile = 0;
+let bubbles = []
+
 
 let w = window.innerWidth;
 let h = window.innerHeight;
@@ -13,7 +16,7 @@ function setup() {
   // button with function record
 	button = createButton('click me');
   button.position(w/2 -30, h/1.25);
-  button.mousePressed(startRec);
+  button.mouseClicked(startRec);
 
   //initiate mic
   mic = new p5.AudioIn();
@@ -32,7 +35,6 @@ function draw(){
 
   // state text function
   if (state === 1){
-    console.log('dit is de state ' + state);
     textSize(30);
     fill('red'); 
     text('recording...', w/2 - 60, h/2);
@@ -42,13 +44,24 @@ function draw(){
     fill('green'); 
     text('done, click to save', w/2 - 60, h/2)
   }
+
+  for (let i = 0; i < bubbles.length; i++) {
+    bubbles[i].move()
+    bubbles[i].show()
+
+  }
+  if (bubbles.length > 20) {
+    bubbles.splice(0, 1)
+  }
 }
 
 function startRec(){
+  
   if (state === 0 && mic.enabled) {
     // record to our p5.SoundFile
     getAudioContext().resume()
     recorder.record(soundFile);
+    console.log('state: ' + state);
     state++; 
   }
   else if (state === 1) {
@@ -59,10 +72,39 @@ function startRec(){
   }
   else if (state === 2) {
     soundFile.play(); // play the result!
-    // save(soundFile, 'mySound.wav');
+    // save(soundFile, numberFile+'.wav');
     state = 0;
+    numberFile++;
+    // function for bubbles
+    pushBubble(numberFile);
   }
 } 
+
+function pushBubble(numberFile){
+  console.log(numberFile);
+  // let r = random(10,40);
+  let b = new Bubble(50, 100, 25)
+  bubbles.push(b)
+}
+
+class Bubble {
+  constructor(x, y, r){
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.sound = numberFile + '.wav';
+  }
+  move(){
+    this.x = this.x + random(-1,2);
+    this.y = this.y + random(-1,2);
+  }
+  show(){
+    stroke('turquoise');
+    strokeWeight(4);
+    noFill();
+    ellipse(this.x, this.y, this.r * 2);
+  }
+}
 
 // resize canvas when browser resize
 // function windowResized() {
