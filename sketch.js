033d3d6af372523ba0bc663560dc1b705eabc484
ParelZalm <62101020+ParelZalm@users.noreply.github.com>
2022-05-05 19:33:02 +0200

    text in draw function

--- sketch.js
+++ sketch.js
@@ -7,13 +7,12 @@ let h = window.innerHeight;
 function setup() {
   //canvas setup
   canvas = createCanvas(w, h);
-  background(50, 55, 100);
   pg = createGraphics(300, 300);
   
 
   // button with function record
 	button = createButton('click me');
-  button.position(w/2, h/1.25);
+  button.position(w/2 -30, h/1.25);
   button.mousePressed(startRec);
 
   //initiate mic
@@ -28,38 +27,46 @@ function setup() {
   soundFile = new p5.SoundFile();
 }
 
-// function draw(){
-
-// }
+function draw(){
+  background(50, 55, 100);
+  if (state === 1){
+    console.log('dit is de state ' + state);
+    textSize(30);
+    fill('red'); 
+    text('recording...', w/2 - 60, h/2);
+  }
+  else if (state === 2){
+    textSize(30);
+    fill('green'); 
+    text('done, click to save', w/2 - 60, h/2)
+  }
+}
 
 function startRec(){
   if (state === 0 && mic.enabled) {
     // record to our p5.SoundFile
+    getAudioContext().resume()
     recorder.record(soundFile);
-    textSize(30);
-    fill('red'); 
-    text('recording...', w/2 - 20, h/2);
-    console.log('hee state is nu 0')
     state++; 
   }
   else if (state === 1) {
-    background(50, 55, 100);
     // stop recorder and
     // send result to soundFile
     recorder.stop();
-    textSize(30);
-    fill('green'); 
-    text('done, click to save', w/2 - 60, h/2)
-    console.log('hee state is nu 1')
     state++;
   }
   else if (state === 2) {
     soundFile.play(); // play the result!
     // save(soundFile, 'mySound.wav');
-    console.log('hee state is nu 2')
-    state = 2;
+    state++;
   }
 } 
+
+// resize canvas when browser resize
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+//   background(50, 55, 100);
+// }
 
 
 
