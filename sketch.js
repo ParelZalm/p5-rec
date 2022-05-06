7779d141757605c32c656dc202672cb51e719002
ParelZalm <thefinestintern@gmail.com>
2022-05-06 14:24:05 +0200

    downgrade for compatibility

--- sketch.js
+++ sketch.js
@@ -3,6 +3,12 @@ let state = 0;
 let numberFile = 0;
 let bubbles = []
 let easing = 0.05;
+let angle = 0;
+let xEllipse = 100;
+let yEllipse = 300;
+let disX = 650;
+let disY = 450;
+let thisSound;
 
 
 let w = window.innerWidth;
@@ -12,12 +18,13 @@ function setup() {
   //canvas setup
   canvas = createCanvas(w, h);
   pg = createGraphics(300, 300);
+  angleMode(DEGREES);
   
 
   // button with function record
-	button = createButton('click me');
-  button.position(w/2 -30, h/1.25);
-  button.mouseClicked(startRec);
+	// button = createButton('click me');
+  // button.position(w/2 -30, h/1.25);
+  // button.mouseClicked(startRec);
 
   //initiate mic
   mic = new p5.AudioIn();
@@ -49,20 +56,21 @@ function draw(){
   }
 
   for (let i = 0; i < bubbles.length; i++) {
-    // bubbles[i].move();
+    bubbles[i].move();
     bubbles[i].show();
     bubbles[i].read(mouseX, mouseY, i);
   }
   if (bubbles.length > 20) {
     bubbles.splice(0, 1)
   }
+  angle++;
 }
 
-function startRec(){
+function mousePressed(){
   
   if (state === 0 && mic.enabled) {
     // record to our p5.SoundFile
-    getAudioContext().resume()
+    // getAudioContext().resume()
     recorder.record(soundFile);
     console.log('state: ' + state);
     state++; 
@@ -75,9 +83,10 @@ function startRec(){
   }
   else if (state === 2) {
     soundFile.play(); // play the result!
-    // save(soundFile, numberFile+'.wav');
+    saveSound(soundFile, numberFile+'.wav');
     state = 0;
-    numberFile++;
+    numberFile += 1;
+    console.log(numberFile)
     // function for bubbles
     pushBubble(numberFile);
   }
@@ -92,29 +101,46 @@ function pushBubble(numberFile){
 
 class Bubble {
   constructor(x, y, r){
-    let ran = random(0,400);
-    this.x = ran;
-    this.y = ran;
+    let ranx = random(0,600);
+    let rany = random(0,600);
+    this.x = ranx;
+    this.y = rany;
+    // this.x = disX + xEllipse * cos(angle);
+    // this.y = disY + yEllipse * sin(angle);
     this.r = r;
-    this.sound = numberFile + '.wav';
+    this.brightness = 0;
+    // this.sound = numberFile + '.wav';
+    this.soundFile = soundFile;
   }
   move(){
-    this.x = this.x += random(-5,5);
-    this.y = this.y += random(-5,5);
+    // this.x = this.x += random(-5,5);
+    // this.y = this.y += random(-5,5);
+    // this.x = disX + xEllipse * cos(angle);
+    // this.y = disY + yEllipse * sin(angle);
   }
   show(){
     stroke('turquoise');
     strokeWeight(4);
-    noFill();
+    fill(this.brightness, 125);
     ellipse(this.x, this.y, this.r * 2);
   }
   read(pixelX, pixelY, i){
+
     let distance = dist(pixelX, pixelY, this.x, this.y);
-    if (distance < this.r && mouseIsPressed === true){
-      console.log(bubbles[i].sound);
+    if (distance < this.r){
+      console.log(bubbles[i]);
+      console.log(i);
+      this.brightness = 255;
+      // this.soundFile.play();
+      // this.soundFile.play();
+      //console.log(this.soundFile);
+      return;
     }
   }
 }
+
+
+
 
 // resize canvas when browser resize
 // function windowResized() {
